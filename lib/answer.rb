71d028acd252611cbbf60f86986fe3b611d4106f
James Mead <james@floehopper.org>
2011-07-20 12:32:46 +0100

    Try to use URI and credentials from environment variables supplied by Connfu add-on.

--- lib/answer.rb
+++ lib/answer.rb
@@ -1,6 +1,8 @@
 require 'connfu'
 
-Connfu.setup "usera@46.137.85.52", "1"
+uri = URI.parse(ENV['CONNFU_URL'])
+jid = "#{uri.userinfo}@#{uri.host}"
+Connfu.setup jid, ENV['CONNFU_PASSWORD']
 Connfu.redis_uri = "redis://redistogo:73d5d351c2595cc21b9d9baef231ccc6@catfish.redistogo.com:9458/"
 
 class AnswerExample
