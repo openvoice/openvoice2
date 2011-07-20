5013d1660d3cbf5d808006fcbf9821dce2f28641
James Mead <james@floehopper.org>
2011-07-20 17:58:18 +0100

    Prepare the app for the new version of the Connfu gem.

--- app/controllers/dials_controller.rb
+++ app/controllers/dials_controller.rb
@@ -4,7 +4,10 @@ class DialsController < ApplicationController
   end
 
   def create
-    Connfu::App.dial(:to => params[:to], :from => params[:from])
+    dsl = Class.new do
+      include Connfu::Dsl
+    end
+    dsl.dial(:to => params[:to], :from => params[:from])
     flash[:notice] = "Dialed #{params[:to]} successfully"
     redirect_to new_dial_path
   end
--- lib/answer.rb
+++ lib/answer.rb
@@ -1,9 +1,11 @@
 require 'connfu'
+require 'connfu/queue/resque'
 
 uri = URI.parse(ENV['CONNFU_JABBER_URI'])
 jid = "#{uri.user}@#{uri.host}"
 Connfu.setup jid, uri.password
-Connfu.redis_uri = "redis://redistogo:73d5d351c2595cc21b9d9baef231ccc6@catfish.redistogo.com:9458/"
+Resque.redis = "redis://redistogo:73d5d351c2595cc21b9d9baef231ccc6@catfish.redistogo.com:9458/"
+# Resque.redis = "redis://localhost:6379"
 
 class AnswerExample
   include Connfu::Dsl
@@ -12,6 +14,8 @@ class AnswerExample
     call.on_answer do
       p "Answered call"
       # check call number to see if I dialled it, or has already got voicemail message recorded....
+
+      sleep 2 # avoid known prism/tropo bug
       say('please record your greeting')
       start_recording
 
