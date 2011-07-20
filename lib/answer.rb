5708f3f09000697deb3c1c5bded9faed5acf01a9
James Mead <james@floehopper.org>
2011-07-20 12:20:54 +0100

    Attempt to answer incoming call using config from Connfu Heroku add-on.

--- lib/answer.rb
+++ lib/answer.rb
@@ -1,28 +1,17 @@
 require 'connfu'
 
-Connfu.setup "usera@46.137.85.52", "1"
+uri = URI.parse(ENV['CONNFU_URL'])
+jid = "#{uri.userinfo}@#{uri.host}"
+Connfu.setup jid, ENV['CONNFU_PASSWORD']
 Connfu.redis_uri = "redis://redistogo:73d5d351c2595cc21b9d9baef231ccc6@catfish.redistogo.com:9458/"
 
 class AnswerExample
   include Connfu::Dsl
 
-  on :outgoing_call do |call|
-    call.on_answer do
-      p "Answered call"
-      # check call number to see if I dialled it, or has already got voicemail message recorded....
-      say('please record your greeting')
-      start_recording
-
-      sleep 5
-
-      p stop_recording
-
-      hangup
-    end
-
-    call.on_ringing do
-      p "Call is ringing"
-    end
+  on :offer do |call|
+    answer
+    say('i am answering your call even though i am just a robot')
+    hangup
   end
 end
 
