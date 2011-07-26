39cb66afb8a9fcd783ec4f6463fba10b33bc9568
James Mead & Jason Cale <pairing+james.mead+jason.cale@gofreerange.com>
2011-07-26 15:24:20 +0100

    For the moment we just want to present the user with a spoken message instead of music.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,7 +5,7 @@ Connfu.config.uri = ENV['CONNFU_JABBER_URI']
 Connfu.start do
   on :offer do |call|
     answer
-    say 'http://www.phono.com/audio/troporocks.mp3'
+    say 'please wait while we transfer your call'
     result = transfer 'sip:jasoncale@iptel.org', 'sip:floehopper@iptel.org'
     puts "The call was answered, and has finished" if result.answered?
   end
