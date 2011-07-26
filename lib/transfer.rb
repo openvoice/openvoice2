7632806e41e446a941f97745a37bc5d8bb66355f
James Mead & Jason Cale <pairing+james.mead+jason.cale@gofreerange.com>
2011-07-26 17:23:58 +0100

    Transfer call to the account's endpoints specified in the database and not just some hard-coded ones.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,10 +4,10 @@ Connfu.config.uri = ENV['CONNFU_JABBER_URI']
 
 Connfu.start do
   on :offer do |call|
-    if Account.where(:username => call.to[:username]).any?
+    if account = Account.find_by_username(call.to[:username])
       answer
       say 'please wait while we transfer your call'
-      result = transfer 'sip:jasoncale@iptel.org', 'sip:floehopper@iptel.org'
+      result = transfer *account.endpoints.map(&:address)
       puts "The call was answered, and has finished" if result.answered?
     end
   end
