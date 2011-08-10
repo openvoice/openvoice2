42a3f9c4c2a86cb7d79a8fa047f2c5d5ba5aecd0
James Mead & Jason Cale <pairing+james.mead+jason.cale@gofreerange.com>
2011-08-10 11:17:47 +0100

    Use the right constant to set the Resque redis_url as per 7daef4ed9b0d8aa63911ba40085dcb88d18d6e81.
    I (James M) take it all back. James A had it right all along.

--- config/initializers/connfu.rb
+++ config/initializers/connfu.rb
@@ -1,3 +1,3 @@
 require 'connfu/queue/resque'
-Connfu::Queue::Resque.redis = ENV["REDISTOGO_URL"] || "redis://localhost:6379"
+Resque.redis = ENV["REDISTOGO_URL"] || "redis://localhost:6379"
 Connfu.config.uri = ENV['CONNFU_JABBER_URI'] || "jid://usera:1@127.0.0.1"
