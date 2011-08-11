86bc90a3f5da320cbdd57f438bf4e134f1442e4e
Chris Roos & Jason Cale <pairing+chris.roos+jason.cale@gofreerange.com>
2011-08-11 15:06:33 +0100

    The web-originated call should hang up when either the caller or callee hangs up.
    The latest version of the connfu gem gives us access to #last_event_call_id which allows us to hang up the call when either the caller or callee hangs up.

--- lib/jobs/outgoing_call.rb
+++ lib/jobs/outgoing_call.rb
@@ -14,15 +14,26 @@ module Jobs
 
       dial :to => caller, :from => connfu_user do |c|
         c.on_answer do
-          command_options = {
-            :call_jid => call_jid,
-            :client_jid => client_jid,
-            :dial_to => recipient,
-            :dial_from => connfu_user,
-            :call_id => call_id
-          }
-          sleep 1
-          send_command Connfu::Commands::NestedJoin.new(command_options)
+          if call_id == last_event_call_id
+            command_options = {
+              :call_jid => call_jid,
+              :client_jid => client_jid,
+              :dial_to => recipient,
+              :dial_from => connfu_user,
+              :call_id => call_id
+            }
+            sleep 1
+            result = send_command Connfu::Commands::NestedJoin.new(command_options)
+            @joined_call_id = result.ref_id
+            observe_events_for(@joined_call_id)
+          end
+        end
+        c.on_hangup do
+          if call_id == last_event_call_id
+            hangup "#{@joined_call_id}@#{Connfu.connection.jid.domain}"
+          else
+            hangup
+          end
         end
       end
     end
