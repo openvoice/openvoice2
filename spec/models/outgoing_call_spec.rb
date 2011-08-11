bb547de293f415bee9490db0cc145f08286dc042
Chris Roos & Jason Cale <pairing+chris.roos+jason.cale@gofreerange.com>
2011-08-11 15:42:47 +0100

    Extended this test to deal with hanging up when the caller hangs up.
    We need to extend this further to really test the arguments to the commands we're sending but this is a good starting point.

--- spec/models/outgoing_call_spec.rb
+++ spec/models/outgoing_call_spec.rb
@@ -12,10 +12,19 @@ describe Jobs::OutgoingCall do
     incoming :outgoing_call_result_iq, "call-id", Connfu.connection.commands.last.id
     incoming :outgoing_call_ringing_presence, "call-id"
     incoming :outgoing_call_answered_presence, "call-id"
+    incoming :outgoing_call_result_iq, "joined-call-id", Connfu.connection.commands.last.id
+    incoming :joined_presence, "call-id", "joined-call-id"
+    incoming :joined_presence, "joined-call-id", "call-id"
+    incoming :outgoing_call_ringing_presence, "joined-call-id"
+    incoming :outgoing_call_answered_presence, "joined-call-id"
+    incoming :unjoined_presence, "call-id", "joined-call-id"
+    incoming :unjoined_presence, "joined-call-id", "call-id"
+    incoming :hangup_presence, "call-id"
 
     Connfu.connection.commands.map(&:class).should == [
       Connfu::Commands::Dial,
-      Connfu::Commands::NestedJoin
+      Connfu::Commands::NestedJoin,
+      Connfu::Commands::Hangup
     ]
   end
 end
