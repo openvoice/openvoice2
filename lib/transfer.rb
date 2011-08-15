3e30dffbd6aea8cc9fe9ba9d09dc17c48d877003
zhao lu <zlu@me.com>
2011-08-15 16:40:40 +0100

    For inbound calls, when user answers one of the ringing legs, all the other legs will hangup.
    However, due to tropo2 issue 141: https://github.com/tropo/tropo2/issues/141
    We do receive an error iq for the leg being hungup.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,27 +9,33 @@ Connfu.start do
 
       # We must do the say before the Join happens. It will end once the join happens.
       result_iq = send_command Connfu::Commands::Say.new(
-        :call_jid => call_jid,
-        :client_jid => client_jid,
-        :text => "http://www.phono.com/audio/troporocks.mp3"
-      )
+                                   :call_jid => call_jid,
+                                   :client_jid => client_jid,
+                                   :text => "http://www.phono.com/audio/troporocks.mp3"
+                               )
 
       command_options = {
-        :call_jid =>call_jid,
-        :client_jid =>client_jid,
-        :dial_from => call.to[:address],
-        :call_id => call_id
+          :call_jid =>call_jid,
+          :client_jid =>client_jid,
+          :dial_from => call.to[:address],
+          :call_id => call_id
       }
 
+      call_ids = []
       account.endpoints.each do |endpoint|
         result = send_command Connfu::Commands::NestedJoin.new(command_options.merge(:dial_to => endpoint.address))
 
         # the call id we are going to JOIN will be returned in the result
         observe_events_for(result.ref_id)
+        call_ids << result.ref_id
       end
 
       # answer event for the joining call will now be handled by this "call".
-      wait_for Connfu::Event::Answered
+      answered_call = wait_for Connfu::Event::Answered
+
+      (call_ids-[answered_call.call_id]).each do |hangup_call_id|
+        hangup "#{hangup_call_id}@#{Connfu.connection.jid.domain}"
+      end
 
       wait_for Connfu::Event::Hangup
       @finished = true
