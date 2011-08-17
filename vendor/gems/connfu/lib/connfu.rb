5e8a0146a1cbb81e3e37747d20cb5f39407d6322
James Mead & Zhao Lu <pairing+james.mead+zhao.lu@gofreerange.com>
2011-08-17 15:05:07 +0100

    Updated connfu gem.

--- vendor/gems/connfu/lib/connfu.rb
+++ vendor/gems/connfu/lib/connfu.rb
@@ -20,58 +20,62 @@ module Connfu
     attr_accessor :event_processor
     attr_accessor :connection
     attr_accessor :io_log
-  end
 
-  def self.handle_stanza(stanza)
-    io_log.received stanza if io_log
-    event = Connfu::Rayo::Parser.parse_event_from(stanza)
-    event_processor.handle_event(event)
-  end
-
-  def self.start(handler_class = nil, &block)
-    logger.info "Tropo build: #{tropo_build}"
-    io_log.info "Tropo build: #{tropo_build}" if io_log
-    handler_class ||= build_handler_class(&block)
-    install_handler(handler_class)
-    EM.run do
-      EventMachine::add_periodic_timer(1, Queue::Worker.new(Jobs::Dial.queue))
-      connection.run
+    def handle_stanza(stanza)
+      io_log.received stanza if io_log
+      event = Connfu::Rayo::Parser.parse_event_from(stanza)
+      event_processor.handle_event(event)
     end
-  end
 
-  def self.config
-    @config ||= Configuration.new
-  end
-
-  def self.connection
-    @connection ||= Connection.new(config)
-  end
-
-  def self.logger
-    @logger ||= Logger.new(STDOUT)
-  end
-
-  def self.tropo_build
-    possible_prism_locations = ['~/Applications/prism', '/Applications/prism', '/opt/voxeo/prism'].map { |p| File.expand_path(p) }
-    prism_home = possible_prism_locations.find { |p| File.directory?(p) }
-    if prism_home && File.exist?(File.join(prism_home, "apps/tropo-build.txt"))
-      File.read(File.join(prism_home, "apps/tropo-build.txt"))
-    else
-      "unknown"
+    def start(handler_class = nil, &block)
+      logger.info "Tropo build: #{tropo_build}"
+      io_log.info "Tropo build: #{tropo_build}" if io_log
+      handler_class ||= build_handler_class(&block)
+      install_handler(handler_class)
+      EM.run do
+        EventMachine::add_periodic_timer(1, Queue::Worker.new(Jobs::Dial.queue))
+        connection.run
+      end
     end
-  end
 
-  private
-
-  def self.build_handler_class(&block)
-    Class.new do
-      include Connfu::Dsl
-      instance_eval(&block)
+    def config
+      @config ||= Configuration.new
     end
-  end
 
-  def self.install_handler(handler_class)
-    self.event_processor = EventProcessor.new(handler_class)
-    self.connection.register_handler(:ready) { handler_class.on_ready }
+    def connection
+      @connection ||= Connection.new(config)
+    end
+
+    def logger
+      @logger ||= Logger.new(STDOUT)
+    end
+
+    def tropo_build
+      possible_prism_locations = ['~/Applications/prism', '/Applications/prism', '/opt/voxeo/prism'].map { |p| File.expand_path(p) }
+      prism_home = possible_prism_locations.find { |p| File.directory?(p) }
+      if prism_home && File.exist?(File.join(prism_home, "apps/tropo-build.txt"))
+        File.read(File.join(prism_home, "apps/tropo-build.txt"))
+      else
+        "unknown"
+      end
+    end
+
+    def finished?
+      event_processor.handlers.all?(&:finished?)
+    end
+
+    private
+
+    def build_handler_class(&block)
+      Class.new do
+        include Connfu::Dsl
+        instance_eval(&block)
+      end
+    end
+
+    def install_handler(handler_class)
+      self.event_processor = EventProcessor.new(handler_class)
+      self.connection.register_handler(:ready) { handler_class.on_ready }
+    end
   end
 end
