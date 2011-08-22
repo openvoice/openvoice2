9ad41f17ff9e570de4e8544902eac604630b45c1
Jason Cale & Zhao Lu, zlu <pair+jasoncale+zhao@openvoice2.com>
2011-08-22 17:37:33 +0100

    Updated Connfu gem and removed debug puts that was left in RecordGreeting by mistake

--- lib/jobs/record_greeting.rb
+++ lib/jobs/record_greeting.rb
@@ -12,15 +12,9 @@ module Jobs
       dial :from => openvoice_number, :to => endpoint_address do |call|
         call.on_answer do
           say 'please record a greeting'
-
           recordings = record_for 10
-
-          p "****** OVNUMBER: " + openvoice_number
-
           account = Account.find_by_username(openvoice_number.match(/^sip:(.*)@.*/)[1])
           account.update_attribute(:greeting_path, recordings.first)
-
-
         end
       end
     end
