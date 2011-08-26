3d2e05da04b808e2492c2dc2602c3202b33bc544
Tom Ward <tom@popdog.net>
2011-08-26 12:47:59 +0100

    Move parallel and round robin call handling into dedicated methods

--- lib/incoming_call.rb
+++ lib/incoming_call.rb
@@ -3,76 +3,23 @@ require 'connfu'
 
 class IncomingCall
   include Connfu::Dsl
+
+  attr_accessor :offer, :call_record
+
   on :offer do |offer|
-    if account = Account.find_by_number(offer.to[:username]) || Account.find_by_username(offer.to[:username])
-      call_record = account.calls.create!(:incoming => true, :party_address => offer.from)
+    self.offer = offer
+
+    if account
+      self.call_record = account.calls.create!(:incoming => true, :party_address => offer.from)
       answer
       say account.greeting_path || 'please wait while we transfer your call'
 
       play_hold_music
 
       if account.parallel_dial?
-        parallel_calls = account.endpoints.inject({}) do |memo, endpoint|
-          call_id = dial_join({:dial_from => offer.to[:address], :dial_to => endpoint.address})
-          memo[call_id] = endpoint
-          memo
-        end
-
-        call_ids = parallel_calls.keys
-
-        answered_event = wait_for_one_leg_to_answer(call_ids)
-
-        if answered_event.instance_of?(Connfu::Event::Answered)
-          call_record.state = Call::ANSWERED
-          call_record.endpoint = parallel_calls[answered_event.call_id]
-          call_record.save!
-
-          unanswered_calls = call_ids - [answered_event.call_id]
-
-          hangup_calls(unanswered_calls)
-
-          answered_call_id = answered_event.call_id
-          hangup_openvoice_leg_when_caller_hangs_up(answered_call_id)
-        end
+        establish_call_using_parallel_dial
       else
-        answered_event = nil
-        endpoint = account.endpoints.detect do |endpoint|
-          result = dial(:from => offer.to[:address], :to => endpoint.address)
-          answered_event = wait_for Connfu::Event::Answered, Connfu::Event::Rejected, :timeout => 10
-          
-          if answered_event.is_a?(Connfu::Event::Answered) ||
-             answered_event.is_a?(Connfu::Event::Rejected)
-            true
-          else
-            hangup "#{result.ref_id}@#{Connfu.connection.jid.domain}"
-            false
-          end
-        end
-
-        case answered_event
-        when Connfu::Event::Answered
-          call_record.state = Call::ANSWERED
-          call_record.endpoint = endpoint
-          call_record.save!
-          
-          wait_because_of_tropo_bug_133
-
-          result = send_command Connfu::Commands::Join.new(
-            :call_jid => call_jid,
-            :client_jid => client_jid,
-            :call_id => answered_event.call_id
-          )
-          wait_for Connfu::Event::Joined
-          logger.debug "Call established"
-
-          answered_call_id = answered_event.call_id
-          hangup_openvoice_leg_when_caller_hangs_up(answered_call_id)
-        else
-          stop_hold_music
-          say "Please leave a message"
-          record_for(60, :beep => true)
-          say "Message complete"
-        end
+        establish_call_using_round_robin
       end
 
       logger.debug "The call has finished"
@@ -80,6 +27,76 @@ class IncomingCall
   end
 
   private
+
+  def account
+    @account ||= Account.find_by_number(offer.to[:username]) || Account.find_by_username(offer.to[:username])
+  end
+
+  def establish_call_using_parallel_dial
+    parallel_calls = account.endpoints.inject({}) do |memo, endpoint|
+      call_id = dial_join({:dial_from => offer.to[:address], :dial_to => endpoint.address})
+      memo[call_id] = endpoint
+      memo
+    end
+
+    call_ids = parallel_calls.keys
+
+    answered_event = wait_for_one_leg_to_answer(call_ids)
+
+    if answered_event.instance_of?(Connfu::Event::Answered)
+      call_record.state = Call::ANSWERED
+      call_record.endpoint = parallel_calls[answered_event.call_id]
+      call_record.save!
+
+      unanswered_calls = call_ids - [answered_event.call_id]
+
+      hangup_calls(unanswered_calls)
+
+      answered_call_id = answered_event.call_id
+      hangup_openvoice_leg_when_caller_hangs_up(answered_call_id)
+    end
+  end
+
+  def establish_call_using_round_robin
+    answered_event = nil
+    endpoint = account.endpoints.detect do |endpoint|
+      result = dial(:from => offer.to[:address], :to => endpoint.address)
+      answered_event = wait_for Connfu::Event::Answered, Connfu::Event::Rejected, :timeout => 10
+
+      if answered_event.is_a?(Connfu::Event::Answered) ||
+         answered_event.is_a?(Connfu::Event::Rejected)
+        true
+      else
+        hangup "#{result.ref_id}@#{Connfu.connection.jid.domain}"
+        false
+      end
+    end
+
+    case answered_event
+    when Connfu::Event::Answered
+      call_record.state = Call::ANSWERED
+      call_record.endpoint = endpoint
+      call_record.save!
+
+      wait_because_of_tropo_bug_133
+
+      result = send_command Connfu::Commands::Join.new(
+        :call_jid => call_jid,
+        :client_jid => client_jid,
+        :call_id => answered_event.call_id
+      )
+      wait_for Connfu::Event::Joined
+      logger.debug "Call established"
+
+      answered_call_id = answered_event.call_id
+      hangup_openvoice_leg_when_caller_hangs_up(answered_call_id)
+    else
+      stop_hold_music
+      say "Please leave a message"
+      record_for(60, :beep => true)
+      say "Message complete"
+    end
+  end
 
   def play_hold_music
     # Not waiting after the command means the music will stop when one of the calls is answered
@@ -90,7 +107,7 @@ class IncomingCall
     )
     @hold_music_component_id = result.ref_id
   end
-  
+
   def stop_hold_music
     send_command Connfu::Commands::Stop.new(
       :call_jid => call_jid,
